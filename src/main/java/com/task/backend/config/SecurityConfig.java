73079ed8788975f25544b5106ec0253dd64ab4a7
Ankush Bisht <111033560+ankubisht987@users.noreply.github.com>
2026-05-02 13:58:06 +0530

    Update SecurityConfig.java

--- src/main/java/com/task/backend/config/SecurityConfig.java
+++ src/main/java/com/task/backend/config/SecurityConfig.java
@@ -4,22 +4,34 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.*;
+
+import java.util.List;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    http
-        .cors()   // important
-        .and()
-        .csrf(csrf -> csrf.disable())
-        .authorizeHttpRequests(auth -> auth
-            .anyRequest().permitAll()
-        )
-        .formLogin(form -> form.disable())
-        .httpBasic(basic -> basic.disable());
 
-    return http.build();
-}
+        http
+            .csrf(csrf -> csrf.disable())
+            .cors(cors -> cors.configurationSource(request -> {
+                CorsConfiguration config = new CorsConfiguration();
+
+                config.setAllowCredentials(true);
+                config.setAllowedOrigins(List.of(
+                    "http://localhost:5173",
+                    "https://task-manager-frontend-5xwuajlmg-sahilrwat9876-6435s-projects.vercel.app"
+                ));
+
+                config.setAllowedHeaders(List.of("*"));
+                config.setAllowedMethods(List.of("*"));
+
+                return config;
+            }))
+            .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+
+        return http.build();
+    }
 }

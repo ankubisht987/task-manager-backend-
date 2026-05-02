34c3c3ad8fa193bd4fe2aafb2cfcc41e70de8bcb
Ankush Bisht <111033560+ankubisht987@users.noreply.github.com>
2026-05-02 12:43:47 +0530

    Create CorsConfig.java

---
+++ src/main/java/com/task/backend/config/CorsConfig.java
@@ -0,0 +1,31 @@
+package com.task.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.*;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.List;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(List.of(
+                "http://localhost:5173",
+                "https://task-manager-frontend-dpf227cy9-sahilrwat9876-6435s-projects.vercel.app"
+        ));
+
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsFilter(source);
+    }
+}

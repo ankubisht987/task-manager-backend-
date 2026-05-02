a54a7c8952932c3cdfaf59164296c7635520c9c5
Ankush Bisht <111033560+ankubisht987@users.noreply.github.com>
2026-05-02 13:26:31 +0530

    Update SecurityConfig.java

--- src/main/java/com/task/backend/config/SecurityConfig.java
+++ src/main/java/com/task/backend/config/SecurityConfig.java
@@ -10,14 +10,16 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll()
-                )
-                .formLogin(form -> form.disable())
-                .httpBasic(basic -> basic.disable());
+    http
+        .cors()   // important
+        .and()
+        .csrf(csrf -> csrf.disable())
+        .authorizeHttpRequests(auth -> auth
+            .anyRequest().permitAll()
+        )
+        .formLogin(form -> form.disable())
+        .httpBasic(basic -> basic.disable());
 
-        return http.build();
-    }
+    return http.build();
+}
 }
